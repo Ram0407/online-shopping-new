dccd02b9785ec08480e0667bab95c9b9588634a8
Ramakrishna Iyyamgaru <iyyamgaruramakrishna@gmail.com>
2024-05-04 00:41:30 +0530

    Introduced Circuit Breaker Pattern in order-service which make calls to inventory-service (for fault tolerance)

--- order-service/src/main/java/com/ram/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ram/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ram.orderservice.controller;
 
 import com.ram.orderservice.dto.OrderRequest;
 import com.ram.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "myFallBackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully...!";
+    }
+
+    public String myFallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! We regret to inform that something went wrong, Please order after sometime";
     }
 
 }
